c5cb4c63e7ff25681faf3a40216bd46a4e70a40d
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-24 07:18:56 +0900

    env setting

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-  database: process.env.NEXT_APP_FIREBASE_DATABASE,
-  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  database: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
