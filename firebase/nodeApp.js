53b933b82523a50e86e2db3b85525e73669ad579
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-25 21:46:16 +0900

    edit firebase setting file

--- firebase/nodeApp.js
+++ firebase/nodeApp.js
@@ -3,7 +3,7 @@ import * as admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: "react-hooks-ts-firebase",
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
       privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
